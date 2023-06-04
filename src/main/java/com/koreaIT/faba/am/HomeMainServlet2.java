04d90227b251ab4a559cc48a17418aa5cd1a6664
CK-0512 <dhcdkrl1004@gmail.com>
2023-06-04 18:57:43 +0900

    서블릿 생성, 파라미터 테스트

---
+++ src/main/java/com/koreaIT/faba/am/HomeMainServlet2.java
@@ -0,0 +1,41 @@
+package com.koreaIT.faba.am;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//-> 요청 주소의 경로를 매핑 -> url 매핑
+@WebServlet("/home/printDan")
+public class HomeMainServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=UTF-8");
+
+		String inputDan = request.getParameter("dan");
+		String inputLimit = request.getParameter("limit");
+		String inputColor = request.getParameter("color");
+		
+		if (inputDan == null)
+			inputDan = "1";
+		if (inputLimit == null)
+			inputDan = "1";
+		if (inputColor == null)
+			inputDan = "black";
+		
+		int dan = Integer.parseInt(inputDan);
+		int limit = Integer.parseInt(inputLimit);
+
+		response.getWriter().append(String.format("<div style=\"color:%s;\">== %d단 ==</div>", inputColor, dan));
+
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("<div style='color:%s;'>%d * %d = %d</div>", inputColor, dan, i, dan * i));
+		}
+	}
+
+}
