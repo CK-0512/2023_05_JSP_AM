59fae0f408bbe78963106575309b7bef059db378
CK-0512 <dhcdkrl1004@gmail.com>
2023-06-11 21:50:55 +0900

    DB접속정보 설정파일

--- src/main/java/com/koreaIT/java/am/ArticleDoDeleteServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleDoDeleteServlet.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
 
+import com.koreaIT.java.am.config.Config;
 import com.koreaIT.java.am.util.DBUtil;
 import com.koreaIT.java.am.util.SecSql;
 
@@ -25,10 +26,10 @@ public class ArticleDoDeleteServlet extends HttpServlet {
 		
 		Connection conn = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/jsp_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+			Class.forName(Config.getDBDriverName());
+			String url = Config.getDBUrl();
 			
-			conn = DriverManager.getConnection(url, "root", "");
+			conn = DriverManager.getConnection(url, Config.getDBUser(), Config.getDBPw());
 			
 			String inputId = request.getParameter("id");
 			int id = Integer.parseInt(inputId);
